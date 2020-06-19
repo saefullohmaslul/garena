999dcf322aad5118cbde4ce357134b485637f475
Saefulloh Maslul <maslulsaefulloh@gmail.com>
2020-06-19 22:28:35 +0700

    feat: add program content

---
+++ src/modules/home/components/program/index.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+import './index.scss'
+
+const ProgramContent = () => {
+  return (
+    <section id="program">
+      <div className="program-title">
+        <p>Undergraduate Scholarship Program</p>
+      </div>
+      <div className="program-content">
+        <div className="program-content-desc">
+          <p>
+            We are excited to announce that Sea Indonesia is now partnering with five universities in Indonesia and offering a merit-based scholarship to bright Indonesian students. This scholarship
+            is our commitment to give back to the community by empowering youth through education. Through this scholarship, we hope to provide talented students with the opportunity to pursue a
+            bachelor’s degree and solve the talent-gap issue in the technology industry in Indonesia.
+          </p>
+        </div>
+        <div className="program-content-items">
+          <div className="card-item"></div>
+          <div className="card-item"></div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default ProgramContent
